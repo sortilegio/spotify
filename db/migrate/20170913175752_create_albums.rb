502191e22a96d5a327bb9de7b72df604474d66b2
sortilegio <johannacastro8407@gmail.com>
2017-09-13 13:11:08 -0500

    modelos relaciones

---
+++ db/migrate/20170913175752_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[5.1]
+  def change
+    create_table :albums do |t|
+      t.string :name
+      t.string :image_url
+      t.string :released_at
+      t.integer :artist_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20170913175859_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :number
+      t.string :preview_url
+      t.integer :album_id
+
+      t.timestamps
+    end
+  end
+end
